6f98dbaffd721ea822b3ba12ac4ad2a930e516f2
Perezented <mperez6473@gmail.com>
2022-07-23 12:58:18 -0500

    Check if month & year are Jul 2022 to do a 20 30 50 split until Aug for it to default to 50-30-10-10 Components broken out, App.js shortened. Button to switch between the types added

---
+++ src/components/NumbersMatch.jsx
@@ -0,0 +1,35 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+export const NumbersMatch = (props) => {
+	const { totalAdded, totalAddedRounded, doNumbersMatch } = props;
+
+  return (
+		<Container className={"border-light mb-3 mt-5 pb-5" + doNumbersMatch()}>
+			<Row>
+				<Col sm={4}>Total added together + rounded:</Col>
+				<Col className="d-flex">
+					<h5>
+						{" " +
+							totalAddedRounded.toFixed(2) +
+							" (" +
+							totalAddedRounded +
+							")"}
+					</h5>
+				</Col>
+			</Row>
+			<Row>
+				<Col sm={4}>Total added together:</Col>
+				<Col className="d-flex">
+					<h5>
+						{" " + parseFloat(totalAdded).toFixed(2) + " (" + totalAdded + ")"}
+					</h5>
+				</Col>
+			</Row>
+			{doNumbersMatch() === " text-success" && totalAdded !== 0 && (
+				<h2 className="my-5 font-weight-bold blink">
+					Numbers match and split evenly
+				</h2>
+			)}
+		</Container>
+	);
+};
---
+++ src/components/SplitTable.jsx
@@ -0,0 +1,76 @@
+import { Table } from "react-bootstrap";
+
+export const SplitTable = (props) => {
+	const { split, fixNumberAndFindPercent, total } = props;
+
+	return (
+		<Table
+			variant="dark"
+			responsive
+			striped={true}
+			bordered={true}
+			className="my-3">
+			<thead>
+				<tr>
+					<th>Percentage Amount</th>
+					<th>Cleanly Total Divided</th>
+					<th>Total Divided</th>
+				</tr>
+			</thead>
+			<tbody>
+				{split ? (
+					<>
+						<tr>
+							<td>
+								<span>20%</span>
+							</td>
+							<td>{fixNumberAndFindPercent(total, 2, 20)}</td>
+							<td className="font-monospace d-flex justify-content-end pe-5">
+								{fixNumberAndFindPercent(total, 6, 20)}
+							</td>
+						</tr>
+					</>
+				) : (
+					<>
+						<tr>
+							<td>
+								<span>10%</span>
+							</td>
+							<td>{fixNumberAndFindPercent(total, 2, 10)}</td>
+							<td className="font-monospace d-flex justify-content-end pe-5">
+								{fixNumberAndFindPercent(total, 6, 10)}
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span>10%</span>
+							</td>
+							<td>{fixNumberAndFindPercent(total, 2, 10)}</td>
+							<td className="font-monospace d-flex justify-content-end pe-5">
+								{fixNumberAndFindPercent(total, 6, 10)}
+							</td>
+						</tr>
+					</>
+				)}
+				<tr>
+					<td>
+						<span>30%</span>
+					</td>
+					<td>{fixNumberAndFindPercent(total, 2, 30)}</td>
+					<td className="font-monospace d-flex justify-content-end pe-5">
+						{fixNumberAndFindPercent(total, 6, 30)}
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<span>50%</span>
+					</td>
+					<td>{fixNumberAndFindPercent(total, 2, 50)}</td>
+					<td className="font-monospace d-flex justify-content-end pe-5">
+						{fixNumberAndFindPercent(total, 6, 50)}
+					</td>
+				</tr>
+			</tbody>
+		</Table>
+	);
+};
